ade0d4fb52518c768fa4f2e520da6bc336fdb33e
PigeonzZz <temperfeazz@gmail.com>
2023-01-24 19:43:05 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    if(-n) {
+        let absolute = Math.abs(n)
+        let toString = String(absolute)
+        let toArray = toString.split('')
+        toArray = toArray.reverse()
+        let arrayToString = toArray.join('')
+        let result = Number(arrayToString)
+        return result
+    } else {
+        let toString = String(n)
+        let toArray = toString.split('')
+        toArray = toArray.reverse()
+        let arrayToString = toArray.join('')
+        let result = Number(arrayToString)
+        return result
+    }
 }
